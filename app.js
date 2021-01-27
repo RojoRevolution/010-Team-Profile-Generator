3191f79b14489e15a36ea87f230d29c1be1e0fd8
David Rojo <rojo@rojorevolution.com>
2021-01-26 21:54:57 -0600

    added 1st engineer option using WHEN, need to see if I can get it to also work with Intern

--- app.js
+++ app.js
@@ -10,10 +10,111 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const initQuestions = [
+    `Team Manager | Enter Name:`,
+    `Team Manager | Enter Employee ID:`,
+    `Team Manager | Enter Email Address:`,
+    `Team Manager | Enter Office Number:`,
+    `Would you like to add another employee?`,
+];
+
+const engineerQuestions = [
+    `New Engineer | Enter Name:`,
+    `New Engineer | Enter Employee ID:`,
+    `New Engineer | Enter Email Address:`,
+    `New Engineer | Enter GitHub URL:`,
+    `What would you like to do next?`,
+]
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const init = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: initQuestions[0],
+                name: "manager-name",
+                validate: function (answer) {
+                    if (answer === "") {
+                        return console.log("A Manager's name is required")
+                    } else {
+                        return true;
+                    }
 
+                }
+
+            },
+            {
+                type: 'input',
+                message: initQuestions[1],
+                name: 'manager-id',
+                validate: function (answer) {
+                    if (answer === "") {
+                        return console.log("An employee ID is required")
+                    } else {
+                        return true;
+                    }
+
+                }
+            },
+            {
+                type: 'input',
+                message: initQuestions[2],
+                name: 'manager-email',
+                validate: function (answer) {
+                    if (answer === "") {
+                        return console.log("An email address is required")
+                    } else {
+                        return true;
+                    }
+
+                }
+            },
+            {
+                type: 'input',
+                message: initQuestions[3],
+                name: 'manager-office',
+                validate: function (answer) {
+                    if (answer === "") {
+                        return console.log("An office number is required")
+                    } else {
+                        return true;
+                    }
+
+                }
+            },
+            {
+                type: 'list',
+                message: initQuestions[4],
+                name: 'addMore',
+                choices: ['Add an Engineer', 'Add an Intern', 'Save and Quit Application']
+            },
+            {
+                type: 'input',
+                message: engineerQuestions[0],
+                name: "engineer-name",
+                // WHEN - this question only runs when the choice to addMore = Add an Engineer
+                when: function (answers) {
+                    return answers.addMore === 'Add an Engineer'
+                },
+                validate: function (answer) {
+                    if (answer === "") {
+                        return console.log("A Manager's name is required")
+                    } else {
+                        return true;
+                    }
+
+                }
+
+            },
+        ]).then((response) => {
+            // Save information
+        })
+}
+
+// function call to initialize program
+init();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
@@ -33,3 +134,5 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+
