0065fd04bf2959eddefe4401beddf81b2e768894
David Rojo <rojo@rojorevolution.com>
2021-01-26 22:12:43 -0600

    trying a different route. Going to set each set of question to their own array and run inquirer based on the array

--- app.js
+++ app.js
@@ -26,95 +26,82 @@ const engineerQuestions = [
     `What would you like to do next?`,
 ]
 
+const internQuestions = [
+    `New Intern | Enter Name:`,
+    `New Intern | Enter Employee ID:`,
+    `New Intern | Enter Email Address:`,
+    `New Intern | Enter School:`,
+    `What would you like to do next?`,
+]
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-const init = () => {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: initQuestions[0],
-                name: "manager-name",
-                validate: function (answer) {
-                    if (answer === "") {
-                        return console.log("A Manager's name is required")
-                    } else {
-                        return true;
-                    }
+const managerQuestion = [
+    {
+        type: 'input',
+        message: initQuestions[0],
+        name: "managerName",
+        validate: function (answer) {
+            if (answer === "") {
+                return console.log("A Manager's name is required")
+            } else {
+                return true;
+            }
 
-                }
+        }
 
-            },
-            {
-                type: 'input',
-                message: initQuestions[1],
-                name: 'manager-id',
-                validate: function (answer) {
-                    if (answer === "") {
-                        return console.log("An employee ID is required")
-                    } else {
-                        return true;
-                    }
+    },
+    {
+        type: 'input',
+        message: initQuestions[1],
+        name: 'managerId',
+        validate: function (answer) {
+            if (answer === "") {
+                return console.log("An employee ID is required")
+            } else {
+                return true;
+            }
 
-                }
-            },
-            {
-                type: 'input',
-                message: initQuestions[2],
-                name: 'manager-email',
-                validate: function (answer) {
-                    if (answer === "") {
-                        return console.log("An email address is required")
-                    } else {
-                        return true;
-                    }
+        }
+    },
+    {
+        type: 'input',
+        message: initQuestions[2],
+        name: 'managerEmail',
+        validate: function (answer) {
+            if (answer === "") {
+                return console.log("An email address is required")
+            } else {
+                return true;
+            }
 
-                }
-            },
-            {
-                type: 'input',
-                message: initQuestions[3],
-                name: 'manager-office',
-                validate: function (answer) {
-                    if (answer === "") {
-                        return console.log("An office number is required")
-                    } else {
-                        return true;
-                    }
+        }
+    },
+    {
+        type: 'input',
+        message: initQuestions[3],
+        name: 'managerOffice',
+        validate: function (answer) {
+            if (answer === "") {
+                return console.log("An office number is required")
+            } else {
+                return true;
+            }
 
-                }
-            },
-            {
-                type: 'list',
-                message: initQuestions[4],
-                name: 'addMore',
-                choices: ['Add an Engineer', 'Add an Intern', 'Save and Quit Application']
-            },
-            {
-                type: 'input',
-                message: engineerQuestions[0],
-                name: "engineer-name",
-                // WHEN - this question only runs when the choice to addMore = Add an Engineer
-                when: function (answers) {
-                    return answers.addMore === 'Add an Engineer'
-                },
-                validate: function (answer) {
-                    if (answer === "") {
-                        return console.log("A Manager's name is required")
-                    } else {
-                        return true;
-                    }
+        }
+    },
+    {
+        type: 'list',
+        message: initQuestions[4],
+        name: 'addMore',
+        choices: ['Add an Engineer', 'Add an Intern', 'Save and Quit Application']
+    },
+];
 
-                }
-
-            },
-        ]).then((response) => {
-            // Save information
-        })
-}
+console.log(managerQuestion);
 
 // function call to initialize program
-init();
+// init();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
