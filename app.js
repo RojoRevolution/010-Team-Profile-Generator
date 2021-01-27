7b5ff9773741299ae6d34ca3c79b1b5b0a0d30f2
David Rojo <rojo@rojorevolution.com>
2021-01-27 17:15:00 -0600

    HTML file is rendering properly. Going to work on adjusting CSS on the template HTML files

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const util = require('util')
 const path = require("path");
 const fs = require("fs");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -253,6 +255,7 @@ function addMore() {
         } else {
             console.log('APPLICATION ENDING')
             console.log(teamMembers)
+            renderAndOutput(teamMembers);
         }
     })
 }
@@ -262,7 +265,11 @@ init();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+async function renderAndOutput(file) {
+    const htmlPage = render(file);
 
+    await writeFileAsync(outputPath, htmlPage);
+}
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
